10fc9f72defcef63d22bf4f85481dadad323c121
Avishag Seneor <avishagseneor@gmail.com>
2024-01-07 14:37:45 +0100

    Ajout du contenu.

--- app/src/main/java/fr/be2/gsb_medicaments/DatabaseHelper.java
+++ app/src/main/java/fr/be2/gsb_medicaments/DatabaseHelper.java
@@ -10,12 +10,14 @@ import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.regex.Pattern;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -98,7 +100,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         selectionArgs.add("%" + denomination + "%");
         selectionArgs.add("%" + formePharmaceutique + "%");
         selectionArgs.add( "%" + titulaires + "%");
-        selectionArgs.add( "%" + denominationSubstance + "%");
+        selectionArgs.add( "%" + removeAccents(denominationSubstance) + "%");
         SQLiteDatabase db = this.getReadableDatabase();
         String finSQL ="";
 
@@ -108,13 +110,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
 
         // La requête SQL de recherche
+        String SQLSubstance = "SELECT CODE_CIS FROM CIS_COMPO_bdpm WHERE replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(upper(Denomination_substance), 'Â','A'),'Ä','A'),'À','A'),'É','E'),'Á','A'),'Ï','I'), 'Ê','E'),'È','E'),'Ô','O'),'Ü','U'), 'Ç','C' ) LIKE ?" ;
+        // La requête SQL de recherche
         String query = "SELECT * FROM CIS_bdpm WHERE " +
                 "Denomination_du_medicament LIKE ? AND " +
                 "Forme_pharmaceutique LIKE ? AND " +
                 "Titulaires LIKE ? AND " +
-                "Code_CIS IN (SELECT Code_CIS FROM CIS_COMPO_bdpm WHERE Denomination_Substance LIKE ?) " +
+                "Code_CIS IN (" +SQLSubstance+ ")" +
                 finSQL;
-
 
         // Les valeurs à remplacer dans la requête
 
@@ -208,6 +211,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
 
     }
+    private String removeAccents(String input) {
+        if (input == null) {
+            return null;
+        }
 
+        // Normalisation en forme de décomposition (NFD)
+        String normalized = Normalizer.normalize(input, Normalizer.Form.NFD);
+
+        // Remplacement des caractères diacritiques
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return pattern.matcher(normalized).replaceAll("");
+    }
 
 }
